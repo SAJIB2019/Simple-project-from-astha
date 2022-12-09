d49b2ce20404f196a28541242eba937357fb7afb
Sajib <sajib.sohelrana21@gmail.com>
2022-12-10 01:55:11 +0600

    10th commit

--- src/routes/copyRight.svelte
+++ src/routes/copyRight.svelte
@@ -2,5 +2,5 @@
     import "../app.css";
 </script>
 <div class="flex justify-center py-2">
-    <p class="text-[18px] font-bold font-lora">Copyright © 2022. All rights reserved.</p>
+    <p class="text-[10px] sm:text-[18px] font-bold font-lora">Copyright © 2022. All rights reserved.</p>
 </div>
